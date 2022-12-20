cd960e0bc806418c9d29e65e94d704779200b3c6
mohitgarg05 <83368553+mohitgarg05@users.noreply.github.com>
2022-12-20 18:15:18 +0530

    first commit

---
+++ pages/contentArray.js
@@ -0,0 +1,40 @@
+const data = [
+    {
+        projectName:"Coders' Room",
+        Description : "Coders' Room is the clone of codepen website where you can write HTML,CSS and JS code. Additionally user's can create room in which they can code live with their friends this live functionality is done by firebase which provides real-time data exchange.You can be in audio room with your friend to talk while coding .Their is a history tab which shows all room you created or joined.",
+        TechSkills: ["ReactJs" , "NodeJs" , "Firebase"],
+        ViewSite : "https://silly-hamilton-e2b0c3.netlify.app/",
+        Github : "https://github.com/mohitgarg05/Code-Editor",
+        color1 : "rgb(160,98,181)",
+        color2 : "rgb(87,206,81)"
+    },
+    {
+        projectName:"Price and Stock Comparison using Web scraping",
+        Description : "This project is team project where my part is to make frontend for this website. • We have developed a price and stock comparison project. In this project we take data from three different websites and then compare their price in our project. We use productId for the comparison of product from different website and after comparison we showed user the price and availability of stock in all three website so that don't have to go to different website to get product for least price.",
+        TechSkills: ["HTML" , "CSS" , "NextJs" , "API"],
+        ViewSite : "",
+        Github : "https://github.com/mohitgarg05/ShoesShopping",
+        color1:"rgb(179,81,184)",
+        color2 :"rgb(203,163,52)"
+    },
+    {
+        projectName:"450DSA",
+        Description : "450DSA is a website which helps user to track their DSA performance. I have take love Babbar 450 dsa sheet to get questions then I make different sections for different topics of dsa and in each section user can see how many they have done and how many and which one are left . This helps user to track their preparation of DSA. I used indexedDB(Browser database) to store the data of individual",
+        TechSkills: ["HTML" , "CSS" , "ReactJs" , "IndexedDB"],
+        ViewSite : "https://mohitgarg.netlify.app/",
+        Github : "https://github.com/mohitgarg05/450DSA",
+        color1 : "rgb(108,115,134)",
+        color2 : "rgb(189,154,57)"
+    },
+    {
+        projectName:"Weather app",
+        Description : "This website is the website to check current weather condition and forecast for next 5 days use openweather api to get data of weather. I make it at time of covid so i also use coronaVirus api to fetch the results of that particular state user searched for.   ",
+        TechSkills: ["HTML" , "CSS" , "ReactJs" , "API"],
+        ViewSite : "https://distracted-meitner-11adf0.netlify.app/",
+        Github : "https://github.com/mohitgarg05/WeatherCoronaUpdate",
+        color1 : "rgb(99,93,212)",
+        color2 : "rgb(140,120,108)"
+    }
+]
+
+export default data;
---
+++ pages/desc.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import style from '../styles/Home.module.css'
+import 'animate.css';
+const desc = (props) => {
+
+    console.log(props)
+  return (
+    <div >
+        <h4>{props.data.projectName}</h4>
+                    <div className={style.aboutMePara}>
+                      <p>{props.data.Description}</p>
+                        <p style={{display:"flex"}}>Tech Used : 
+                        {props.data.TechSkills.map((item)=>{
+                          return (
+                              <p className={style.removeMargin} style={{marginLeft :"20px",padding:"0px"}}>{item}</p>
+                          )
+                        })}</p>
+                        <div className={style.components}>
+                            <a href={props.data.ViewSite} target="_blank">View Site</a>
+                            <a href={props.data.Github} target="_blank" className={style.second}>GitHub</a>
+                        </div>
+                    </div>
+    </div>
+  )
+}
+
+export default desc
